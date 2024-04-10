33f776a696c8be57f335e9e3a67742ce93a0e5d0
Matias <mati.delarosa1984@gmail.com>
2024-04-10 13:33:34 -0300

    user model

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,25 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+// rest of the packages
+const morgan = require('morgan');
 
 // database
-const connectDB = require('./db/connect')
+const connectDB = require('./db/connect');
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('e-commerce api')
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
