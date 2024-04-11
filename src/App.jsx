0c49ae6c2aa90a8137ddb53be8aa8081883ef9f1
Matias <mati.delarosa1984@gmail.com>
2024-04-11 15:36:41 -0300

    signed and secure flags

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(morgan('tiny'));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser(process.env.JWT_SECRET));
 
 app.get('/api/v1', (req, res) => {
   res.send('e-commerce api')
