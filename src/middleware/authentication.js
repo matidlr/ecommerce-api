512debc688f7c917bf2b0fd33635ec88da9340fd
Matias <mati.delarosa1984@gmail.com>
2024-04-11 22:10:59 -0300

    authenticate user complete

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,13 +1,16 @@
-const CustomErr = require('../errors');
+const CustomError = require('../errors');
 const {isTokenValid} = require('../utils')
 
 const authenticateUser = async (req,res,next) =>{
   const token = req.signedCookies.token;
   if(!token){
-    console.log('error, no token present')
-  }else{
-
-    console.log('token present');
+    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+  }
+  try {
+    const { name, userId, role } = isTokenValid({token});
+    req.user = { name, userId, role };
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError('Authentication Invalid');
   }
   next();
 };
