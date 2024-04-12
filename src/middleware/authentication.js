3e244cb1e0ce037660f66c83f7269eab6d3298c9
Matias <mati.delarosa1984@gmail.com>
2024-04-11 22:36:59 -0300

    authorize permissions

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -9,12 +9,22 @@ const authenticateUser = async (req,res,next) =>{
   try {
     const { name, userId, role } = isTokenValid({token});
     req.user = { name, userId, role };
+    next();
   } catch (error) {
     throw new CustomError.UnauthenticatedError('Authentication Invalid');
   }
-  next();
 };
+
+const authorizePermissions = (req, res, next) =>{
+  if(req.user.role !== 'admin'){
+    throw new CustomError.UnauthorizedError(
+      'Unauthorized to access this route'
+    )
+  }
+ next();
+}
 
 module.exports = {
   authenticateUser,
+  authorizePermissions,
 }
