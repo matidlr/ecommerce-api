de028d1db3a507a45d5bef2c98402f1d43a7ee7e
Matias <mati.delarosa1984@gmail.com>
2024-04-14 12:47:24 -0300

    product routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,9 @@ const cookieParser = require('cookie-parser');
 const connectDB = require('./db/connect');
 
 // routers
-const authRouter = require('./routes/authRoutes')
-const userRouter = require('./routes/userRoutes')
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes')
 
 // middleware
 const notFoundMiddleware = require('./middleware/not-found');
@@ -29,6 +30,7 @@ app.get('/api/v1', (req, res) => {
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
