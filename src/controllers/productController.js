bd653b46aa7dc17a0a66f0dde2454439f2c69f85
Matias <mati.delarosa1984@gmail.com>
2024-04-14 16:41:34 -0300

    uploadImage controller

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const path = require('path');
 
 const createProduct = async(req, res) =>{
   req.body.user = req.user.userId;
@@ -49,7 +50,25 @@ const deleteProduct = async(req, res) =>{
   res.status(StatusCodes.OK).json({ msg: 'Success! Product removed.'})
 };
 const uploadImage = async(req, res) =>{
-  res.send('upload image');
+  if(!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded')
+  }
+  const productImage = req.files.image;
+
+  if(!productImage.mimetype.startsWith('image')){
+     throw new CustomError.BadRequestError('Please Upload Image');
+  }
+
+const maxSize = 1024 * 1024;
+
+if(producttImage > maxSize) {
+  throw new CustomError.BadRequestError(
+    'Please upload image smaller than 1MB'
+  );
+}
+const imagePath = path.join(_dirname,'../public/uploads' + `${productImage.name}`);
+await productImage.mv(imagePath);
+res.status(StatusCodes.OK).json({image:`/uploads/${productImage.name}`})
 };
 
 module.exports = {
