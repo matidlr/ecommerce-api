49d062f86c13912c5dbd7208b8cd972db98a8192
Matias <mati.delarosa1984@gmail.com>
2024-04-14 20:12:17 -0300

    review routes structure

---
+++ src/controllers/reviewController.js
@@ -0,0 +1,25 @@
+
+
+const createReview = async (req, res) => {
+  res.send('create review')
+};
+const getAllReviews = async (req, res) => {
+  res.send('get all reviews')
+};
+const getSingleReview = async (req, res) => {
+  res.send('get single review review')
+};
+const updateReview = async (req, res) => {
+  res.send('update review')
+};
+const deleteReview = async (req, res) => {
+  res.send('delete review')
+};
+
+module.exports = {
+  createReview, 
+  getAllReviews, 
+  getSingleReview, 
+  updateReview, 
+  deleteReview,
+};
